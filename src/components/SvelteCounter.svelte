8b08ff0f53edcf0b93aed70a0b30c6fd6fcccba0
vhollo <vhollo@protonmail.com>
2021-11-17 02:06:26 +0100

    Initial co

--- src/components/SvelteCounter.svelte
+++ src/components/SvelteCounter.svelte
@@ -11,7 +11,15 @@
 </script>
 
 <div id="svelte" class="counter">
-  <button on:click={subtract}>-</button>
+  <button on:click={subtract}>–</button>
   <pre>{ count }</pre>
   <button on:click={add}>+</button>
 </div>
+
+<style>
+  button {
+    transform: scale(1.5);
+    border: 12px double black;
+    cursor:pointer;
+  }
+</style>
